e9f58654accc8a5c9195db72c8b966e717a631c7
D416-26\user <jejunuuer@jejunu.ac.kr>
2023-04-17 15:03:26 +0900

    modify pow function to check base

--- ctrl.py
+++ ctrl.py
@@ -31,11 +31,4 @@ class Control:
         return a/b
     
     def pow(self, a, b):
-        try:
-           if (a==0):
-                raise Exception("Base Error")
-            
-        except Exception as e:
-            return e
-        
-        return pow(a, b) 
+        return pow(a, b)
